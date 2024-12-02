6e1b13150190749100bb78d39d19d825ef34d489
RE045908 <re045908@qintess.com>
2024-12-02 19:51:55 -0300

    Improve testing

--- src/test/java/com/qikserve/supermarket/service/impl/BasketServiceImplTest.java
+++ src/test/java/com/qikserve/supermarket/service/impl/BasketServiceImplTest.java
@@ -1,22 +1,34 @@
 package com.qikserve.supermarket.service.impl;
 
+import com.github.tomakehurst.wiremock.WireMockServer;
+
+import com.qikserve.supermarket.client.ProductService;
 import com.qikserve.supermarket.enuns.TypePromotion;
 import com.qikserve.supermarket.model.Product;
 import com.qikserve.supermarket.model.Promotion;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.mockito.Mockito.*;
-
 
 class BasketServiceImplTest {
 
@@ -32,72 +44,241 @@ class BasketServiceImplTest {
     @Mock
     private QtyBasedPriceOverrideServiceImpl qtyBasedPriceOverrideServiceImpl;
 
+    @InjectMocks
+    private WireMockServer wireMockServer;
+
+    @InjectMocks
+    private ProductService productService;
+
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
         basketService.create();
+        wireMockServer = new WireMockServer(8081);
+        wireMockServer.start();
+        configureFor("localhost", 8081);
+        RestTemplate restTemplate = new RestTemplate();
+        productService = new ProductService(restTemplate);
+    }
+
+    @AfterEach
+    void teardown() {
+        if (wireMockServer != null) {
+            wireMockServer.stop();
+        }
     }
 
     @Test
-    void calculateTotalCostApplyingPromotionNoPromotion() {
-        this.buildTwoProductionNoPromotion();
+    void calculateTotalCostApplyingPromotionNoPromotion() throws URISyntaxException, IOException {
+
+        String mockResponse = new String(
+                Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("twoBoringFriesProduct.json").toURI()))
+        );
+        stubFor(get(urlPathMatching("/products/.*"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(mockResponse)));
+        List<Map<String, Object>> twoBoringFriesProduct = this.productService.fetchProductById("4MB7UfpTQs");
+
+        this.buildTwoProductionNoPromotion(twoBoringFriesProduct);
+
+
         Double totalCost = basketService.calculateTotalCostApplyingPromotion(1L);
 
         assertEquals(398.00, totalCost);
     }
 
     @Test
-    void calculateTotalCostApplyingWithPromotionBuyXGetYFree() {
+    void calculateTotalCostApplyingWithPromotionBuyXGetYFree() throws URISyntaxException, IOException {
 
-        this.buildTwoProductionWithPromotionBuyXGetYFree();
-        when(buyXGetYFreeServiceImpl.applyDiscount(eq(buildBurgerProduct()), any(), eq(2)))
+        String mockResponse = new String(
+                Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("twoBurgerProduct.json").toURI()))
+        );
+        stubFor(get(urlPathMatching("/products/.*"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(mockResponse)));
+        List<Map<String, Object>> twoBurgerProduct = this.productService.fetchProductById("PWWe3w1SDU");
+
+        var product = this.buildTwoProductionWithPromotionBuyXGetYFree(twoBurgerProduct);
+
+        when(buyXGetYFreeServiceImpl.applyDiscount(eq(product), any(), eq(2)))
                 .thenReturn(999.00);
+
         Double totalCost = basketService.calculateTotalCostApplyingPromotion(1L);
 
         assertEquals(999.00, totalCost);
     }
 
     @Test
-    void calculateTotalCostApplyingWithPromotionFlatPercent() {
+    void calculateTotalCostApplyingWithPromotionFlatPercent() throws URISyntaxException, IOException {
 
-        this.buildTwoProductionWithPromotionFlatPercent();
-        when(flatPercentServiceImpl.applyDiscount(eq(buildSaladProduct()), any(), eq(2)))
+        String mockResponse = new String(
+                Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("twoSaladProduct.json").toURI()))
+        );
+        stubFor(get(urlPathMatching("/products/.*"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(mockResponse)));
+        List<Map<String, Object>> twoSaladProduct = this.productService.fetchProductById("C8GDyLrHJb");
+
+        var product = this.buildTwoProductionWithPromotionFlatPercent(twoSaladProduct);
+
+        when(flatPercentServiceImpl.applyDiscount(eq(product), any(), eq(2)))
                 .thenReturn(898.2);
+
         Double totalCost = basketService.calculateTotalCostApplyingPromotion(1L);
 
         assertEquals(898.2, totalCost);
     }
 
     @Test
-    void calculateTotalCostApplyingWithPromotionQtyBasedPriceOverride() {
+    void calculateTotalCostApplyingWithPromotionQtyBasedPriceOverride() throws URISyntaxException, IOException {
 
-        this.buildTwoProductionWithPromotionQtyBasedPriceOverride();
-        when(qtyBasedPriceOverrideServiceImpl.applyDiscount(eq(buildPizzaProduct()), any(), eq(2)))
+        String mockResponse = new String(
+                Files.readAllBytes(Paths.get(getClass().getClassLoader().getResource("twoPizzaProduct.json").toURI()))
+        );
+        stubFor(get(urlPathMatching("/products/.*"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody(mockResponse)));
+
+        List<Map<String, Object>> twoPizzaProduct = this.productService.fetchProductById("Dwt5F7KAhi");
+        var product = this.buildTwoProductionWithPromotionQtyBasedPriceOverride(twoPizzaProduct);
+
+        when(qtyBasedPriceOverrideServiceImpl.applyDiscount(eq(product), any(), eq(2)))
                 .thenReturn(1799.00);
         Double totalCost = basketService.calculateTotalCostApplyingPromotion(1L);
 
         assertEquals(1799.00, totalCost);
     }
 
-    private void buildTwoProductionWithPromotionQtyBasedPriceOverride(){
-        basketService.addItem(buildPizzaProduct(), 1L);
-        basketService.addItem(buildPizzaProduct(), 1L);
+    private Product buildTwoProductionWithPromotionQtyBasedPriceOverride(List<Map<String, Object>> productMaps){
+        Product product = null;
+        for (Map<String, Object> productMap : productMaps) {
+            product = mapToPizzaProduct(productMap);
+            basketService.addItem(product,1L);
+        }
+        return product;
     }
 
-    private void buildTwoProductionWithPromotionFlatPercent(){
-        basketService.addItem(buildSaladProduct(), 1L);
-        basketService.addItem(buildSaladProduct(), 1L);
+//    private void buildTwoProductionWithPromotionQtyBasedPriceOverride(){
+//        basketService.addItem(buildPizzaProduct(), 1L);
+//        basketService.addItem(buildPizzaProduct(), 1L);
+//    }
+
+//    private void buildTwoProductionWithPromotionFlatPercent(){
+//        basketService.addItem(buildSaladProduct(), 1L);
+//        basketService.addItem(buildSaladProduct(), 1L);
+//    }
+
+//    private void buildTwoProductionWithPromotionBuyXGetYFree(){
+//        basketService.addItem(buildBurgerProduct(), 1L);
+//        basketService.addItem(buildBurgerProduct(), 1L);
+//    }
+
+    private Product buildTwoProductionWithPromotionFlatPercent(List<Map<String, Object>> productMaps){
+        Product product = null;
+        for (Map<String, Object> productMap : productMaps) {
+            product = mapToSaladProduct(productMap);
+            basketService.addItem(product,1L);
+        }
+        return product;
     }
 
-    private void buildTwoProductionWithPromotionBuyXGetYFree(){
-        basketService.addItem(buildBurgerProduct(), 1L);
-        basketService.addItem(buildBurgerProduct(), 1L);
+    private Product buildTwoProductionWithPromotionBuyXGetYFree(List<Map<String, Object>> productMaps){
+        Product product = null;
+        for (Map<String, Object> productMap : productMaps) {
+            product = mapToBurgerProduct(productMap);
+            basketService.addItem(product,1L);
+        }
+        return product;
     }
 
-    private void buildTwoProductionNoPromotion(){
-        basketService.addItem(buildBoringFriesProduct(), 1L);
-        basketService.addItem(buildBoringFriesProduct(), 1L);
+    private static Product mapToPizzaProduct(Map<String, Object> productMap) {
+        List<Promotion> promotions = new ArrayList<>();
+        List<Map<String, Object>> promotionsList = (List<Map<String, Object>>) productMap.get("promotions");
+        if (promotionsList != null) {
+            for (Map<String, Object> promotionMap : promotionsList) {
+                Promotion promotion = Promotion.builder()
+                        .id((String) promotionMap.get("id"))
+                        .typePromotion(TypePromotion.valueOf((String) promotionMap.get("type")))
+                        .amount((Integer) promotionMap.get("required_qty"))
+                        .price((Double) promotionMap.get("price"))
+                        .build();
+                promotions.add(promotion);
+            }
+        }
+
+        return Product.builder()
+                .id((String) productMap.get("id"))
+                .name((String) productMap.get("name"))
+                .price(((Number) productMap.get("price")).doubleValue())
+                .promotions(promotions)
+                .build();
     }
+
+    private static Product mapToBurgerProduct(Map<String, Object> productMap) {
+        List<Promotion> promotions = new ArrayList<>();
+        List<Map<String, Object>> promotionsList = (List<Map<String, Object>>) productMap.get("promotions");
+        if (promotionsList != null) {
+            for (Map<String, Object> promotionMap : promotionsList) {
+                Promotion promotion = Promotion.builder()
+                        .id((String) promotionMap.get("id"))
+                        .typePromotion(TypePromotion.valueOf((String) promotionMap.get("type")))
+                        .requiredQty((Integer) promotionMap.get("required_qty"))
+                        .freeQty((Integer) promotionMap.get("free_qty"))
+                        .build();
+                promotions.add(promotion);
+            }
+        }
+
+        return Product.builder()
+                .id((String) productMap.get("id"))
+                .name((String) productMap.get("name"))
+                .price(((Number) productMap.get("price")).doubleValue())
+                .promotions(promotions)
+                .build();
+    }
+
+    private static Product mapToSaladProduct(Map<String, Object> productMap) {
+        List<Promotion> promotions = new ArrayList<>();
+        List<Map<String, Object>> promotionsList = (List<Map<String, Object>>) productMap.get("promotions");
+        if (promotionsList != null) {
+            for (Map<String, Object> promotionMap : promotionsList) {
+                Promotion promotion = Promotion.builder()
+                        .id((String) promotionMap.get("id"))
+                        .typePromotion(TypePromotion.valueOf((String) promotionMap.get("type")))
+                        .amount((Integer) promotionMap.get("amount"))
+                        .build();
+                promotions.add(promotion);
+            }
+        }
+
+        return Product.builder()
+                .id((String) productMap.get("id"))
+                .name((String) productMap.get("name"))
+                .price(((Number) productMap.get("price")).doubleValue())
+                .promotions(promotions)
+                .build();
+    }
+
+    private void buildTwoProductionNoPromotion(List<Map<String, Object>> productMaps){
+        for (Map<String, Object> productMap : productMaps) {
+            Product product = Product.builder()
+                    .id((String) productMap.get("id"))
+                    .name((String) productMap.get("name"))
+                    .price(((Number) productMap.get("price")).doubleValue())
+                    .promotions((List<Promotion>) productMap.get("promotions"))
+                    .build();
+            basketService.addItem(product,1L);
+        }
+    }
+
+//    private void buildTwoProductionNoPromotion(){
+//        basketService.addItem(buildBoringFriesProduct(), 1L);
+//        basketService.addItem(buildBoringFriesProduct(), 1L);
+//    }
 
     private Product buildBoringFriesProduct() {
         return Product.builder()
@@ -161,5 +342,4 @@ class BasketServiceImplTest {
                 .typePromotion(TypePromotion.QTY_BASED_PRICE_OVERRIDE)
                 .build();
     }
-
 }
